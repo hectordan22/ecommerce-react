736507b95061875e1920fad3ca5f38e109e59ba1
Hector Salas <salsegovi21@gmail.com>
2024-05-20 20:47:48 -0400

    fix(init): inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const name = 'ecomerce'
+const [saludo, setSaludo] = useState('')
+
+ const saluda = () => {
+    setSaludo('Es un gusto que nos visites')
+ }
+  return (
+    <>
+     <h1>Nuestro Proyecto de React {name}</h1>
+      <h4>{saludo}</h4>
+     <button onClick={saluda}>Saluda</button>
+    </>
+  )
+}
+
+export default App
