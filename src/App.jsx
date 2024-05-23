9b5f4e4e673600894188c799453e56f8e69cca28
dfsoto <dfsoto7@gmail.com>
2024-05-22 20:32:19 -0500

    firts change daniel

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- const name = 'ecomerce'
+const name = 'ecomerce'
 const [saludo, setSaludo] = useState('')
 
- const saluda = () => {
-    setSaludo('Es un gusto que nos visites')
- }
+const saluda = () => {
+  setSaludo('Es un gusto que nos visites')
+}
   return (
     <>
-     <h1>Nuestro Proyecto de React {name}</h1>
+      <h1>Nuestro Proyecto de React {name}</h1>
+      <h4>El saludo es el siguiente:</h4>
       <h4>{saludo}</h4>
-     <button onClick={saluda}>Saluda</button>
+      <button onClick={saluda}>Saluda</button>
     </>
   )
 }
-
+   
 export default App
